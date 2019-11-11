7100da440d82656b145dd0c971e19df2382f9ad3
bortolotomaria <47746480+bortolotomaria@users.noreply.github.com>
2019-11-11 10:14:28 -0300

    Update lista1.php

--- duda/lista1.php
+++ duda/lista1.php
@@ -1,22 +1,54 @@
+EU NAO TENHO CERTEZA DE NENHUMA (DA 5 MUITO MENOS)
 
+2-
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-$n1=10;
-$n2=4;
-$n3=8;
-$n4=6; 
-$soma=($n1+$n2+$n3+$n4)/4;
-if ($soma >=6){
-    echo "aprovado";
-   
-}else {
-    echo "reprovado";
+function pegarCupomPorNome($id) {
+    $sql = "SELECT * FROM usuario WHERE nome= $Nome";
+    $resultado = mysqli_query(conn(), $sql);
+    $Cupom = mysqli_fetch_assoc($resultado);
+    return $Cupom;
 }
+
+3-
+n sei
+    
+5- <?php
+function adicionar($id)
+{
+    if (!isset($_SESSION["carrinho"])) {
+        $_SESSION["carrinho"] = array();
+    }
+
+$id = $_POST["id"];
+$nome = $_POST["nome"];
+$descricao = $_POST["descricao"];
+$preco = $_POST["preco"];
+ alert(adicionarUsuario($id, $nome, $descricao, $preco));
+    
+redirecionar("carrinho");
+}
+
 ?>
 
+6-
+<h2>Cupons</h2>
 
+<table class="table">
+    <thead>
+        <tr>
+            <th>Cupom</th>
+            <th>Desconto</th>
+            <th>DELETE</th>
+    </tr>
+        </tr>
+    </thead>
+    <?php foreach ($cupom as $cupom): ?>
+    <tr>
+        <td><?=$cupom['cupom']?></td>
+        <td><?=$cupom['Desconto']?></td>
+        <td><a href="./cupom/deletarCupomComID/<?=$cupom['id']?>" class="btn btn-danger">del</a></td>
+    </tr>
+    <?php endforeach; ?>
+</table>
+
+<a href="./cupom/adicionarNovoCupom" class="btn btn-primary">Adicionar novo cupom</a>
